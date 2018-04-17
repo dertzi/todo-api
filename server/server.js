f79d1966bf7ec00842624585744be1cc99b40b37
Andre D <andre.derghokasian@gmail.com>
2018-04-17 14:56:09 +0300

    Added POST /todos route

--- server/server.js
+++ server/server.js
@@ -7,6 +7,21 @@ let {User} = require('./models/user');
 
 let app = express();
 
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+	let todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((doc) => {
+		res.send(doc);
+	}, (err) => {
+		res.status(400).send(err);
+	});
+});
+
+
 app.listen(3000, () => {
 	console.log('Starter on port 3000');
 });
