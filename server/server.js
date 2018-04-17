d6945761cd514976e02c273e2f995106342eb7e3
Andre D <andre.derghokasian@gmail.com>
2018-04-17 14:44:17 +0300

    Configured basic server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
+
+let app = express();
+
+app.listen(3000, () => {
+	console.log('Starter on port 3000');
+});
