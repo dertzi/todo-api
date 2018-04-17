5ab7e78c2e6c9e7a9f8207612066b0563f70baef
Andre D <andre.derghokasian@gmail.com>
2018-04-17 17:14:17 +0300

    Test POST /todos route. Update .gitignore. Formatted code.

--- server/server.js
+++ server/server.js
@@ -1,27 +1,29 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 
-let {mongoose} = require('./db/mongoose');
-let {Todo} = require('./models/todo');
-let {User} = require('./models/user');
+let { mongoose } = require('./db/mongoose');
+let { Todo } = require('./models/todo');
+let { User } = require('./models/user');
 
 let app = express();
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-	let todo = new Todo({
-		text: req.body.text
-	});
+    let todo = new Todo({
+        text: req.body.text
+    });
 
-	todo.save().then((doc) => {
-		res.send(doc);
-	}, (err) => {
-		res.status(400).send(err);
-	});
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
 });
 
 
 app.listen(3000, () => {
-	console.log('Starter on port 3000');
+    console.log('Starter on port 3000');
 });
+
+module.exports = { app };
