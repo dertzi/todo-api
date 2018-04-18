ac285e9b996b603b834dfeed3a4bca6492b4b914
Andre D <andre.derghokasian@gmail.com>
2018-04-18 14:55:06 +0300

    Added route for DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -32,20 +32,36 @@ app.get('/todos', (req, res) => {
 });
 
 app.get('/todos/:id', (req, res) => {
-	let id = req.params.id;
+    let id = req.params.id;
 
-	if (!ObjectID.isValid(id)) {
-		res.status(404).send();
-	}
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
 
-	Todo.findById(id).then((todo) => {
-		if (!todo) {
-			return res.status(404).send();
-		}
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
 
-		res.send({todo});
-	}).catch((e) => res.status(400).send());
+        res.send({ todo });
+    }).catch((e) => res.status(400).send());
 
+});
+
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+       return res.status(404).send({error: "Invalid ID"});
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send({error: "Todo not in database"});
+        }
+
+        res.send({ todo });
+    }).catch((e) => res.status(400).send());
 });
 
 app.listen(port, () => {
